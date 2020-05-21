6be6ab7b7836ddc3ef0fb28c0a298b5f23201294
TuyenLe <congtuyen598@gmail.com>
2020-05-21 12:28:37 +0700

    add checking for money/bank-detail API

--- controllers/money-transfer.controller.js
+++ controllers/money-transfer.controller.js
@@ -15,15 +15,14 @@ payload.filed2 = 'you idiot';
 // load key from PEM string
 const pubKeyRSA = new nodeRSA(rsaPublicKey);
 const priKeyRSA = new nodeRSA(rsaPrivateKey);
-console.log('pubkey', pubKeyRSA.isEmpty());
-console.log('prikey', priKeyRSA.isEmpty());
+// console.log('pubkey', pubKeyRSA.isEmpty());
+// console.log('prikey', priKeyRSA.isEmpty());
 
 // Hash kiem tra goi tin nguyen ban hay khong
 // Create signature
 // header.sig header.ts body.json
 // make API request using axios
 const date = Date.now().toString();
-const secret = 'TUB';
 // console.log('md5', md5(date + secret).toString());
 
 //test JWT
@@ -48,23 +47,42 @@ const verifyOptions = {
   algorithms: ['RS256'],
 };
 
-jwt.verify(token, rsaPublicKey, verifyOptions, (err, payload) => {
-  if (err) {
-    console.log('verify error', err.name, err.message);
-  } else {
-    // console.log('verified', payload);
-  }
-});
+// jwt.verify(token, rsaPublicKey, verifyOptions, (err, payload) => {
+//   if (err) {
+//     console.log('verify error', err.name, err.message);
+//   } else {
+//     console.log('verified', payload);
+//   }
+// });
 
 // decode, don't need public key
 const decoded = jwt.decode(token, { complete: true });
 // console.log('decoded', decoded);
 
+const auth_bank = ['RSA-bank', 'PGP-bank']; // just test
+const secret = 'hiphopneverdie69';
+
 // implement
+function checkSecurity(req) {
+  const { bank_code, sig, ts } = req.headers;
+  // check partner code
+  if (!auth_bank.includes(bank_code)) throw new Error('Your bankCode is not correct.');
+  // check time in 2 minutes
+  if (Date.now() - parseInt(ts) > 120) throw new Error('Time exceed.');
+  // check signature
+  sigString = bank_code + ts.toString() + JSON.stringify(req.body) + secret;
+  const hash = md5(sigString).toString();
+  if (sig !== hash) throw new Error('Signature failed.');
+}
 
 module.exports = {
   bankDetail: (req, res) => {
-    console.log(req.headers);
+    try {
+      checkSecurity(req);
+      console.log('SUCCESS bank-detail');
+    } catch (err) {
+      console.log('ERROR', err.message);
+    }
     res.send('bank detail here');
   },
   moneyTransfer: (req, res) => {
