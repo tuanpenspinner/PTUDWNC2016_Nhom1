91bd82ae3a0b05e40be4193c59c7a4ad32e20411
catuyen <hoanguyen13317@gmail.com>
2020-06-05 02:24:40 +0700

    update model deal

--- models/deal.model.js
+++ models/deal.model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const typeSchema= mongoose.Schema({
     name: {type: String, required: true, enum: ["internal","transfer","receive"]},  //internal: nội bộ, transfer: gửi tiền đến ngân hàng khác: receive: nhận tiền từ ngân hàng khác
-    bankCode: {type: String, default: "", required: true},
+    bankCode: {type: String, required: true},   //mã ngân hàng, nội bộ là mã ngân hàng của nhóm
 },{
     _id: false
 });
@@ -10,19 +10,20 @@ const dealSchema = new Schema({
     receiverAccountNumber: { type: String, required: true },    //stk thanh toán của người nhận
     transfererAccountNumber: { type: String, required: true },  //stk thanh toán của người gửi
     time: {type: Date,default: null //thời gian chuyển tiền
-        //, required: true
+        , required: true
     },
     amount: { type: Number, required: true },   //số tiền gửi đi
     content: {type: String, trim: true},    //nội dung chuyển tiền
     isTransfered:{type: Boolean,default: false, required: true},    //check đã gửi
+    payFeeBy: {type: String, required: true, enum: ['receiver', 'transferer']}, //hình thức trả phí (người gửi hoặc người nhận trả phí)
     type: {type:typeSchema, required: true},    //loại giao dịch
 });
 
 const Deal = mongoose.model('Deal', dealSchema, 'deals');
 module.exports={
-addDeal : async (receiverAccountNumber, transfererAccountNumber, time,amount,content,isTransfered,type) => {
+addDeal : async (receiverAccountNumber, transfererAccountNumber, time,amount,content,isTransfered,payFeeBy,type) => {
     try {
-        const deal = await Deal.create({receiverAccountNumber, transfererAccountNumber, time,amount,content,isTransfered,type});
+        const deal = await Deal.create({receiverAccountNumber, transfererAccountNumber, time,amount,content,isTransfered, payFeeBy,type});
 
         return deal;
     } catch (e) {
