af203befc6de6c06b0aa196e37ec437c89cd32fa
Tuan <tuanpenspinnner@gmail.com>
2020-06-06 14:46:21 +0700

    Add middleware auth employee and admin

--- routes/customer.js
+++ routes/customer.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const authCustomer =require('../middleware/auth.customer')
+// const authCustomer =require('../middleware/auth.customer')
+// const authEmployee =require('../middleware/auth.employee')
 const customerController= require("../controllers/customer.controller");
 router.get("/list-customer", customerController.getAllCustomers);
 router.get("/:accountNumber", customerController.getCustomer);
-router.get("/register", customerController.registerCustomer);
-router.get("/login", customerController.loginCustomer);
+router.post("/register", customerController.registerCustomer);//Api đăng kí tài khoản customer
+router.post("/login", customerController.loginCustomer);//Api đăng nhập của customer
+router.post("/changePassword", customerController.changePasswordCustomer);//Api thay đổi mật khẩu
 module.exports = router;
