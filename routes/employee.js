b1c139e0084e390521026cf4cbfb2c9b175e9e7f
catuyen <hoanguyen13317@gmail.com>
2020-07-02 03:34:41 +0700

    update route of Employee

--- routes/employee.js
+++ routes/employee.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const employeeController = require("../controllers/employee.controller");
 const authEmployee = require("../middleware/auth.employee");
-router.post("/money-recharge", employeeController.rechargeMoney);
+router.post("/money-recharge",authEmployee, employeeController.rechargeMoney);
 router.post("/login", employeeController.loginEmployee); //Api đăng nhập của employee
 router.get("/info/profile", authEmployee, employeeController.getEmployeeInfo); //Lấy thông tin employee
 router.get("/account-customers",authEmployee,employeeController.getAllAccountCustomers);
